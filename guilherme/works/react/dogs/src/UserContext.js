76067a1f88d0177cf20b334ab0de62abe552d1cb
guilherme dos santos pereira <46527405+guiilhermesp@users.noreply.github.com>
2022-04-03 23:09:55 -0300

    Creating the Api and UserContext components

---
+++ guilherme/works/react/dogs/src/UserContext.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
+import { useNavigate } from 'react-router-dom';
+
+export const UserContext = React.createContext();
+
+export const UserStorage = ({ children }) => {
+  const [data, setData] = React.useState(null);
+  const [login, setLogin] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem('token');
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error('Invalid token!');
+          await getUser(token);
+          navigate('/conta');
+        } catch (err) {
+          userLogout();
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    setData(json);
+    setLogin(true);
+  }
+
+  async function userLogin(username, password) {
+    try {
+      setError(null);
+      setLoading(true);
+      const { url, options } = TOKEN_POST({ username, password });
+      const tokenResponse = await fetch(url, options);
+      if (!token.ok) throw new Error(`Error: ${tokenResponse.statusText}`);
+      const { token } = await tokenResponse.json();
+      window.localStorage.setItem('token', token);
+      await getUser(token);
+    } catch (err) {
+      setError(err.message);
+      setLogin(false);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function userLogout() {
+    setData(null);
+    setError(null);
+    setLoading(false);
+    setLogin(false);
+    window.localStorage.removeItem('token');
+    navigate('/login');
+  }
+
+  return (
+    <UserContext.Provider
+      value={{ userLogin, userLogout, data, error, loading, login }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
